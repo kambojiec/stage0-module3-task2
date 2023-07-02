0a6f1069745c312e3008f76b08f143d4349e6f75
Vlad Kupchik <vladkupchik15@gmail.com>
2023-07-02 22:07:08 +0300

    1

--- src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
+++ src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
@@ -2,13 +2,17 @@ package school.mjc.stage0.module3.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int variable = 0;
-        int variable = 2000;
-        int value = 1000;
-        int variable = 4000;
-        int value2 = 3000;
-        int variables = 5000;
-        variables = 7000;
-        int value = 90000;
+        int zero= 0;
+        int two_thousand = 2000;
+        int one_thousand = 1000;
+        int four_thousand = 4000;
+        int three_thousand = 3000;
+        int five_thousand = 5000;
+        int seven_thousand = 7000;
+        int ninety_thousand = 90000;
+        int one_hundred_thousand = 100000;
+        int million = 1000000;
+        int my_var = 1234567;
+        int my_integer = 1234567;
     }
 }
